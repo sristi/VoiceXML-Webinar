2cbb4a035c3a91156bf953c76583c89d8ee0b4b0
Tim Lytle <tim@timlytle.net>
2015-02-24 11:43:50 -0500

    Simple Hello

---
+++ public/vxml/index.php
@@ -0,0 +1,8 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
+<vxml version = "2.1">
+    <form>
+        <block>
+            <prompt>Hello from Nex mo</prompt>
+        </block>
+    </form>
+</vxml>
